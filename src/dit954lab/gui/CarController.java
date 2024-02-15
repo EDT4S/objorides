e14a9aafaa3c179cea60b906fdcab26042ea36be
EDT4S <guspenged@student.gu.se>
2024-02-15 12:09:47 +0100

    3.1: The small changes connecting the UI to the interface

--- src/dit954lab/gui/CarController.java
+++ src/dit954lab/gui/CarController.java
@@ -2,6 +2,7 @@ package dit954lab.gui;
 
 import dit954lab.world.vehicles.Car;
 import dit954lab.world.vehicles.Volvo240;
+import vector2d.Coord;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -26,7 +27,7 @@ public class CarController {
     // The frame that represents this instance View of the MVC pattern
     CarView frame;
     // A list of cars, modify if needed
-    //ArrayList<Car> cars = new ArrayList<>();
+    ArrayList<Car> cars = new ArrayList<>();
 
     //methods:
 
@@ -34,7 +35,7 @@ public class CarController {
         // Instance of this class
         CarController cc = new CarController();
 
-        //cc.cars.add(new Volvo240());
+        cc.cars.add(new Volvo240(new Coord(0,100),0));
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
@@ -48,23 +49,22 @@ public class CarController {
     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
- /*           for (ACar car : cars) {
+            for (Car car : cars) {
                 car.move();
                 int x = (int) Math.round(car.getPosition().getX());
                 int y = (int) Math.round(car.getPosition().getY());
                 frame.drawPanel.moveit(x, y);
                 // repaint() calls the paintComponent method of the panel
                 frame.drawPanel.repaint();
-            }*/
+            }
         }
     }
 
     // Calls the gas method for each car once
     void gas(int amount) {
         double gas = ((double) amount) / 100;
-       /* for (ACar car : cars
-                ) {
+        for (Car car : cars) {
             car.gas(gas);
-        }*/
+        }
     }
 }
